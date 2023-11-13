e3740aaef2951b41343f7534c3d5d2454cdcafe2
Dina Boshnaq <r0823887@student.thomasmore.be>
2023-11-13 15:13:33 +0100

    starting file

---
+++ samples.py
@@ -0,0 +1,13 @@
+from mss import mss
+
+with mss.mss() as sct:
+    # The screen part to capture
+    monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
+    output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+
+    # Grab the data
+    sct_img = sct.grab(monitor)
+
+    # Save to the picture file
+    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+    print(output)
