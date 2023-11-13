7fd2f85c2e39a8cbb1b9f432089afb4eaad56179
Dina Boshnaq <r0823887@student.thomasmore.be>
2023-11-13 15:34:39 +0100

    half the screen

--- samples.py
+++ samples.py
@@ -1,8 +1,9 @@
-from mss import mss
+import mss.tools
 
 with mss.mss() as sct:
+    monitor_info = sct.monitors[0]
     # The screen part to capture
-    monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
+    monitor = {"top": monitor_info["top"] + monitor_info["height"] // 2, "left": monitor_info["left"], "width": monitor_info["width"], "height": monitor_info["height"] // 2}
     output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
 
     # Grab the data
@@ -11,3 +12,5 @@ with mss.mss() as sct:
     # Save to the picture file
     mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
     print(output)
+
+    
