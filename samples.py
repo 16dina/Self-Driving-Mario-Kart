a017c12795ed2786412f6998222f9e3a6b119338
Dina Boshnaq <r0823887@student.thomasmore.be>
2023-11-13 16:12:07 +0100

    half the screen

--- samples.py
+++ samples.py
@@ -6,7 +6,7 @@ time.sleep(10)
 with mss.mss() as sct:
     monitor_info = sct.monitors[0]
     top_margin = monitor_info["height"] // 3
-    monitor = {"top": monitor_info["top"] + top_margin, "left": monitor_info["left"], "width": monitor_info["width"], "height": monitor_info["height"] * 2 // 3}
+    monitor = {"top": monitor_info["top"] + top_margin, "left": monitor_info["left"], "width": monitor_info["width"], "height": (monitor_info["height"] * 2 // 3)-55}
 
     for i in range(1, 21):
         output = f"{i}.png".format(**monitor)
