67ee22f9b9d3d70602bd33392d1c11aec6105978
Dina Boshnaq <r0823887@student.thomasmore.be>
2023-11-13 16:08:28 +0100

    fix

--- samples.py
+++ samples.py
@@ -1,16 +1,16 @@
 import mss.tools
+import time
+
+time.sleep(10)
 
 with mss.mss() as sct:
     monitor_info = sct.monitors[0]
-    # The screen part to capture
-    monitor = {"top": monitor_info["top"] + monitor_info["height"] // 2, "left": monitor_info["left"], "width": monitor_info["width"], "height": monitor_info["height"] // 2}
-    output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+    top_margin = monitor_info["height"] // 3
+    monitor = {"top": monitor_info["top"] + top_margin, "left": monitor_info["left"], "width": monitor_info["width"], "height": monitor_info["height"] * 2 // 3}
 
-    # Grab the data
-    sct_img = sct.grab(monitor)
-
-    # Save to the picture file
-    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-    print(output)
-
-    
+    for i in range(1, 21):
+        output = f"{i}.png".format(**monitor)
+        sct_img = sct.grab(monitor)
+        mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+        print(f"Captured {output}")
+        time.sleep(1)
