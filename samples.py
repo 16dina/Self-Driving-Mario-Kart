27d1e9726712954c41dc3c2d518c1625d13c6932
Dina Boshnaq <r0823887@student.thomasmore.be>
2023-11-20 14:39:46 +0100

    more work

--- samples.py
+++ samples.py
@@ -3,16 +3,16 @@ import time
 import uuid
 import keyboard
 
-time.sleep(10)
+time.sleep(5)
 
 with mss.mss() as sct:
     monitor_info = sct.monitors[0]
-    top_margin = monitor_info["height"] // 3
-    monitor = {"top": monitor_info["top"] + top_margin, "left": monitor_info["left"] + 55, "width": monitor_info["width"] - 120, "height": (monitor_info["height"] * 2 // 3)-55}
+    top_margin = (monitor_info["height"] // 3) + 50
+    monitor = {"top": monitor_info["top"] + top_margin, "left": monitor_info["left"] + 55, "width": monitor_info["width"] - 120, "height": (monitor_info["height"] * 2 // 3)-105}
 
     # hold space for a bit when you're stopping so it stops
     while not keyboard.is_pressed("space"):
-        output = f"dataset/{str(uuid.uuid4())}.png".format(**monitor)
+        output = f"testingDataset/{str(uuid.uuid4())}.png".format(**monitor)
         sct_img = sct.grab(monitor)
         mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
         print(f"Captured {output}")
